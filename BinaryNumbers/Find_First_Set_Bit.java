3e11896e6f53fa9b1f30b3b7ad756316ba8fe419
Mohammad Asif Lone <110804685+Loneasif88@users.noreply.github.com>
2023-09-19 13:09:35 +0530

    Update and rename Find_First_Set_Bit to Find_First_Set_Bit.java

--- BinaryNumbers/Find_First_Set_Bit.java
+++ BinaryNumbers/Find_First_Set_Bit.java
@@ -1,30 +1,5 @@
 //Question LInk:
-
 "https://practice.geeksforgeeks.org/problems/find-first-set-bit-1587115620/1"
-
-//{ Driver Code Starts
-//Initial Template for Java
-
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-class Main {
-    
-    
-	public static void main (String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int t=sc.nextInt();//taking testcases
-		while(t-->0){
-			int n=sc.nextInt();//input n
-			Solution ob=new Solution();
-			System.out.println(ob.getFirstSetBit(n));//calling method
-		}
-	}
-}
-
-// } Driver Code Ends
-
 
 //User function Template for Java
 
